b0b9c770a43f916a68d4d48207775903a77d58fd
mannyanebi <anebiemmanuel@gmail.com>
2018-11-01 15:46:03 +0100

    Included test cse for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,11 +4,21 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+//creating a seed data that will be added into the database because
+//the beforeEach function removes all documents from the database
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}];
+
 //this should run before the set of test cases in the describe block
 //What this should do is to delete all contents of the data base.
 //this executes before any it() in program.
 beforeEach(function (done) {
     Todo.remove({}).then(function () {
+        return Todo.insertMany(todos);
+    }).then(function () {
         done();
     });
 });
@@ -34,7 +44,9 @@ describe('POST /todos', function () {
                     return done(err); //if theres an error, from any expect case, then the function should end returning the done with the err object
                 }
                 //if no error, test should proceed to check the response from the server, if it corresponse with the result from the text variable
-                Todo.find().then(function (todos) { //return a promise with all the documents from the todos database
+                Todo.find({
+                    text: text
+                }).then(function (todos) { //return a promise with all the documents from the todos database
                     expect(todos.length).toBe(1); //expects the results from the find() that gets all the documents from the database, which is supossed to be just 1
                     expect(todos[0].text).toBe(text); // expecting the return result which is an array with an object, accessing its text property to be same as text
                     done(); //ending the function which is supposed to be an asynchronous function. 
@@ -55,7 +67,7 @@ describe('POST /todos', function () {
                 }
 
                 Todo.find().then(function (todos) {
-                    expect(todos.length).toBe();
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(function (err) {
                     done(err);
@@ -64,4 +76,15 @@ describe('POST /todos', function () {
     });
 }
 );
-//
+
+describe('GET /todos', function () {
+    it('should get all todos', function (done) {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(function (res) {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
