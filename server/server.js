802a9f77ea92efdf43d379713e880367ac43927f
mannyanebi <anebiemmanuel@gmail.com>
2018-11-13 13:12:50 +0100

    Added config file so that condition for running either test or development is checked

--- server/server.js
+++ server/server.js
@@ -1,24 +1,4 @@
-const env = process.env.NODE_ENV || 'development';
-
-// if(process.env.TESTPERMIT == 'test'){
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-    
-// } else{
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-// }
-
-console.log('ENV **********',  env);
-if (env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';     
-}
-
-
+require('./../config/config');
 //library imports
 const _ = require('lodash');
 const express = require('express');
